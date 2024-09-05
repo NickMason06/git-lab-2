844e415ddbc3b89c15b857d22043772fc820ce3e
Nick <nm460323@ohio.edu>
2024-09-05 16:27:40 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nick Mason>
+ *        Date: <09/05/24>
+ * Description: Program to test using Github through the terminal
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
